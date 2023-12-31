3b2c6c496d132ecf73bcc92e556d5acfc6afbba8
unknown <rvchandrasekar5@gmail.com>
2023-12-31 10:56:25 +0530

    list

---
+++ list.py
@@ -0,0 +1,16 @@
+# List
+
+a=[1,2,3,4,5]
+b=[6,7,8,9,10]
+print(a)
+print(type(a))
+a.append(7) # inserting at the end.
+print(a)
+a.insert(0,0)#insertion at the exact position
+print(a)
+a[1]=6 # changing the value of the elements
+print(a)
+a.pop(4) # Extracting the element
+print(a)
+a.extend(b)
+print(a)
