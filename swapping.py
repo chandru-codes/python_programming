f474db18e32632ccdf273cdffc7abd51fdd187c6
chandru-codes <85859740+chandru-codes@users.noreply.github.com>
2024-01-09 07:40:17 +0530

    swapping.py

--- swapping.py
+++ swapping.py
@@ -5,3 +5,13 @@ temp=list1[0]
 list1[0]=list1[-1]
 list1[-1]=temp
 print(list1)
+
+#Given a list in Python and provided the positions of the elements, write a program to swap the two elements in the list. 
+#Input : List1 = [23, 65, 19, 90]
+#Output : [19, 65, 23, 90]
+
+list1=[23,65,19,90]
+temp=list1[0]
+list1[0]=list1[2]
+list1[2]=temp
+print(list1)
